ff2cc0bf28eb859ca8ca97cac8e4397af678e26c
VLuob <1652525703@qq.com>
2021-12-22 17:23:29 +0800

    12.22

--- src/components/Qrcode.vue
+++ src/components/Qrcode.vue
@@ -1,42 +1,47 @@
 <template>
   <div class="qrcode">
-      <img class="close_btn" src="../assets/close.png" alt="" @click="close">
-      <img class="qrcode_img" src="../assets/qrcode_bbt_tech_1.jpg" alt="">
+    <img class="close_btn" src="../assets/close.png" alt="" @click="close" />
+    <img class="qrcode_img" src="../assets/qrcode_bbt_tech_1.jpg" alt="" />
+    <button class="btn" @click="$emit('setFov')">跳转场景</button>
   </div>
 </template>
 <script>
-    export default {
-        name:'qrcode',
-        methods:{
-            close(){
-                this.$emit('closeorder',false)
-            }
-        }
+export default {
+  name: "qrcode",
+  methods: {
+    close() {
+      this.$emit("closeorder", false);
     }
+  }
+};
 </script>
 <style scoped>
-.qrcode{
-    width:100%;
-    height:100%;
-    position: absolute;
-    z-index: 4000;
-    background: rgba(40,40,40,0.6);
+.qrcode {
+  width: 100%;
+  height: 100%;
+  position: absolute;
+  z-index: 4000;
+  background: rgba(40, 40, 40, 0.6);
 }
-.qrcode .close_btn{
-    width:40px;
-    height: 40px;
-    position: absolute;
-    top:10px;
-    left:10px;
+.qrcode .close_btn {
+  width: 40px;
+  height: 40px;
+  position: absolute;
+  top: 10px;
+  left: 10px;
 }
-.qrcode_img{
-    width: 200px;
-    height: 200px;
-    position: absolute;
-    top:50%;
-    left:50%;
-    margin-left:-100px;
-    margin-top:-100px;
+.qrcode_img {
+  width: 200px;
+  height: 200px;
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  margin-left: -100px;
+  margin-top: -100px;
+}
+.btn {
+  position: absolute;
+  right: 50px;
+  top: 0;
 }
 </style>
-
--- src/components/Vtour.vue
+++ src/components/Vtour.vue
@@ -1,82 +1,120 @@
 <template>
   <div id="wrapper">
-      <img class="img_btn" src="../assets/qrcode.png" alt="" v-if="!show" @click="show=!show">
-      <transition
-        name="qrcode-transition"
-        enter-active-class="showUp"
-        leave-active-class="hideDown"
-      >
-        <qrcode v-if="show" @closeorder="getorder"></qrcode>
-      </transition>
-      <div id="pano"></div>
+    <img
+      class="img_btn"
+      src="../assets/qrcode.png"
+      alt=""
+      v-if="!show"
+      @click="show = !show"
+    />
+    <transition
+      name="qrcode-transition"
+      enter-active-class="showUp"
+      leave-active-class="hideDown"
+    >
+      <div>
+        <qrcode v-if="show" @closeorder="getorder" @setFov="setFov"></qrcode>
+      </div>
+    </transition>
+    <div id="pano"></div>
   </div>
 </template>
 <script>
-    import Qrcode from '@/components/Qrcode'
-    export default {
-        name:'vtour',
-        data(){
-            return {
-                show:false
-            }
-        },
-        mounted(){
-            embedpano({swf:"./static/vtour/tour.swf", xml:"./static/vtour/tour.xml", target:"pano", html5:"auto", mobilescale:1.0, passQueryParameters:true});
-        },
-        methods:{
-            getorder(msg){
-                this.show = msg
-            }
-        },
-        components:{
-            Qrcode
-        }
+import Qrcode from "@/components/Qrcode";
+export default {
+  name: "vtour",
+  data() {
+    return {
+      show: false,
+      krpano: null //krpano实例
+    };
+  },
+  mounted() {
+    embedpano({
+      swf: "./static/vtour/tour.swf",
+      xml: "./static/vtour/tour.xml",
+      target: "pano",
+      html5: "auto",
+      mobilescale: this.selectmobilescaleusage(),
+      onready: this.krpano_onready_callback,
+      passQueryParameters: true
+    });
+  },
+  methods: {
+    krpano_onready_callback(krpano_interface) {
+      this.krpano = krpano_interface;
+    },
+    getorder(msg) {
+      this.show = msg;
+    },
+    setFov() {
+      this.krpano.call("goto(scene2)"); //js跳转场景
+      //set(variable, value) 设置属性值
+      //get(variable) 获取属性值
+      //call(action) 调用xml中的各种方法
+      // var fov = Number(krpano.get("view.fov"));
+      // console.log(fov);
+      // fov += 10.0;
+      // krpano.set("view.fov", fov);
+    },
+    selectmobilescaleusage() {
+      // check for Android MQQBrowser:
+      if (
+        navigator.userAgent.indexOf("Android") >= 0 &&
+        navigator.userAgent.indexOf("YYB") >= 0
+      )
+        return 0.5;
+
+      // for all other cases use 1:
+      return 1;
     }
+  },
+  components: {
+    Qrcode
+  }
+};
 </script>
 <style scoped>
-#wrapper{
-    width: 100%;
-    height: 100%;
-    position: absolute;
+#wrapper {
+  width: 100%;
+  height: 100%;
+  position: absolute;
 }
-#pano{
-    width: 100%;
-    height: 100%;
+#pano {
+  width: 100%;
+  height: 100%;
 }
-.img_btn{
-    width:60px;
-    height: 60px;
-    position: absolute;
-    top:10px;
-    right:6px;
-    z-index:4010;
+.img_btn {
+  width: 60px;
+  height: 60px;
+  position: absolute;
+  top: 10px;
+  right: 6px;
+  z-index: 4010;
 }
-.showUp{
-    animation:showup 0.4s ease-in;
+.showUp {
+  animation: showup 0.4s ease-in;
 }
-.hideDown{
-    animation:hidedown 0.4s ease-out;
+.hideDown {
+  animation: hidedown 0.4s ease-out;
 }
 
 @keyframes showup {
-    from {
-        transform:translateY(110%);
-        opacity: 0;
-    }
-    to {
-        transform: translateY(0);
-        opacity: 1;
-    }
-    
+  from {
+    transform: translateY(110%);
+    opacity: 0;
+  }
+  to {
+    transform: translateY(0);
+    opacity: 1;
+  }
 }
 @keyframes hidedown {
-    from {
-        transform: translateY(0);
-    }
-    to {
-        transform: translateY(110%);
-    }
-    
+  from {
+    transform: translateY(0);
+  }
+  to {
+    transform: translateY(110%);
+  }
 }
 </style>
-
